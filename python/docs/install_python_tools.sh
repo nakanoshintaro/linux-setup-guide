3f8483598145d4ce0d2bb6aab224a296aa4a85ce
nakanoshintaro <sn.ice0510@gmail.com>
2026-03-24 22:52:42 +0900

    fix sh

--- python/docs/install_python_tools.sh
+++ python/docs/install_python_tools.sh
@@ -5,6 +5,6 @@ set -e
 echo "🐍 Python と開発ツールをインストールします..."
 
 sudo apt update
-sudo apt install -y python3 python3-pip python3-venv python3-setuptools python3-wheel python3-dev     build-essential ipython black flake8 mypy
+sudo apt install -y python3 python3-pip python3-venv python3-setuptools python3-wheel python3-dev build-essential ipython black flake8 mypy
 
 echo "✅ Python 開発環境のインストールが完了しました。"
